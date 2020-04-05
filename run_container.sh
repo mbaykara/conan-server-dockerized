746e81ae0647bada46fe4ad171637a6338f15e74
Mehmet Ali Baykara <mehmetalibaykara@gmail.com>
2020-04-06 00:47:33 +0200

    Update run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -6,6 +6,6 @@ conan_net=$(docker network create --subnet=172.20.0.0/16 conan_net)
 # build the docker image 
 docker build -t mosaiq_conan_server .
 
-#run container on port 9300
+#run container on port 9300 with persistent mount
 docker run -t -p 9300:9300 --net $conan_net --ip 172.20.0.22 --name conan-server --mount type=bind,source="$(pwd)"/,target=/app mosaiq_conan_server
 
